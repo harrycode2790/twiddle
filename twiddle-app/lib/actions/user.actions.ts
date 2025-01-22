8ba9ef4895385430c8b294d23eaafab47d8ae8cf
harry <hillarybond10@gmail.com>
2025-01-22 14:46:31 +0100

    creating user actions

---
+++ twiddle-app/lib/actions/user.actions.ts
@@ -0,0 +1,27 @@
+'use server'
+
+import User from "../models/user.model"
+import {connectDB } from '../mongoose'
+
+interface CreateUserParams {
+    userId : string;
+    email : string;
+    username : string;
+    name : string;
+    image : string;
+
+}
+
+export const CreateUser = async ( {userId, email, username, name, image}: CreateUserParams) : Promise<void> => {
+
+    try {
+      connectDB()  
+      await User.create({
+            id: userId,
+            username:username?.toLowerCase(),
+            
+      })
+    } catch (err : any) {
+        throw new Error (`Failed to create user${err.message}`)
+    }
+}
